43f8578431ae9fb326e60d125e878bd77dd6fe8e
Joseph C B <josephcb581@gmail.com>
2025-01-02 10:41:52 +0530

    Update readme.md and create the basic functionalities such as grid creation, resizing and clearing

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Etch-a-Sketch
+# Etch-A-Sketch
+
+A simple Etch-A-Sketch web application built using HTML, CSS, and JavaScript.
+
+## Features
+
+- 16x16 grid that can be cleared and resized.
+- Drawing functionality with mouse movements.
+- Simple and intuitive user interface.
+
+## Demo
+
+![Etch-A-Sketch Demo](demo.gif)
+
+## Technologies Used
+
+- HTML
+- CSS (Flexbox for layout)
+- JavaScript
+
---
+++ scripts/index.js
@@ -0,0 +1,43 @@
+let pixelLength = 16;
+
+createGrids();
+
+function createGrids(){
+    let boxSide = 500 / pixelLength;
+    console.log(boxSide);
+    const noOfBoxes = pixelLength ** 2;
+    console.log(noOfBoxes);
+    const container = document.querySelector('.js-container');
+    for(let i=0; i<noOfBoxes; i++){
+        const subBox = document.createElement('div');
+        subBox.setAttribute(`style`, `height: ${boxSide}px; width: ${boxSide}px`);
+        subBox.classList.add("sub-div", "js-sub-div");
+        subBox.addEventListener('mouseover', ()=>{
+            subBox.style.backgroundColor = "black";
+        });
+        container.appendChild(subBox);
+    }
+}
+
+const changeGrid = document.querySelector('.js-grid-change-button');
+
+changeGrid.addEventListener('click', ()=>{
+    changeGridSize(); 
+});
+
+function changeGridSize() {
+    newSize = Number(prompt("Enter a new grid size: (1-100): "));
+    if (newSize<0 || newSize > 100){
+        alert("Please keep the range between 0 and 100");
+        changeGridSize();
+    }else {
+        pixelLength = newSize;
+        clearScreen();
+        createGrids();
+    }
+}
+
+function clearScreen(){
+    const container = document.querySelector('.js-container');
+    container.innerHTML ='';
+}
